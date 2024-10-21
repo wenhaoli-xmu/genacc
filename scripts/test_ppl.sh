f7e453c7aa74cca0ea0fd68267dd2df88647bd37
lwh <1023443389@qq.com>
2024-10-21 02:01:47 +0000

    增加了lm_eval的scripts & 增加了llama2-7b-chat的训练脚本

--- scripts/test_ppl.sh
+++ scripts/test_ppl.sh
@@ -1,5 +1,9 @@
-python test_ppl/test.py --env_conf test_ppl/maskout_0.98.json
-python test_ppl/test.py --env_conf test_ppl/maskout_0.95.json
-python test_ppl/test.py --env_conf test_ppl/maskout_0.90.json
-python test_ppl/test.py --env_conf test_ppl/maskout_0.80.json
-python test_ppl/test.py --env_conf test_ppl/llama2-7b.json
+test_scripts=("maskout_0.98.json" "maskout_0.95.json" "maskout_0.90.json" "maskout_0.80.json" "llama2-7b.json")
+
+for test_script in "${test_scripts[@]}"
+do
+    echo "Running test for ${test_script}..."
+    python test_ppl/test.py --env_conf "test_ppl/${test_script}"
+    echo "Finished processing ${test_script}."
+    echo "-----------------------------------"
+done
